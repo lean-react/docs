9b31161a718c5eb73e90f2cfc1fcc7feebd07580
Michael Alt <mail@michael-alt.info>
2020-03-29 16:30:17 +0200

    Add some navigation ideas.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,22 @@
+import Link from 'next/link';
+
 import '../styles/base.css';
 
 const DocsApp = ({ Component, pageProps }) => (
   <>
-    <nav className="bg-gray-900 text-white px-6 py-4 text-xl">
+    <nav className="text-white bg-gray-900 px-6 py-4 flex justify-between">
       <div>
-        <span>Lean React</span>
+        <Link href="/">
+          <a className="text-xl text-teal-200 hover:text-white">Lean React</a>
+        </Link>
+      </div>
+      <div className="flex">
+        <Link href="/">
+          <a className="mr-4">Dokumente</a>
+        </Link>
+        <Link href="/">
+          <a className="">Tipps</a>
+        </Link>
       </div>
     </nav>
     <Component {...pageProps} />
